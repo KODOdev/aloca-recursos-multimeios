0ee96eaa0fea44213a9bb7035e2b7441a8996421
Daniel Gads <daniel.sousa@fnde.gov.br>
2017-11-13 17:14:52 -0200

    Removed folder from repository

--- aloca-recursos-multimeios/GoldMiner Tech Solutions/mdl_credenciaisAcessoBancoMYSQL.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports MySql.Data.MySqlClient
-
-Module mdl_credenciaisAcessoBancoMYSQL
-    Public Function credenciaisMYSQL() As MySqlConnection
-        'Passa as credenciais para acessar o bd
-        Dim sql As String = "server=127.0.0.1;user id=root; password=admin;database=db_aloca_recursos"
-        Return New MySqlConnection(sql)
-    End Function
-End Module
--- aloca-recursos-multimeios/GoldMiner Tech Solutions/mdl_fechaConexaoBancoMYSQL.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports MySql.Data.MySqlClient
-
-Module mdl_fechaConexaoBancoMYSQL
-    Public Sub fechaConexaoMYSQL()
-        Using con As MySqlConnection = credenciaisMYSQL()
-            con.Close()
-        End Using
-    End Sub
-End Module
